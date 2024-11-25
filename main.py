4aa8e6cc9ec7a849cab741e449043c312f4915e2
usmanadaudu <adekunleakorede101@gmail.com>
2024-11-25 17:35:50 +0100

    catching exceptions

--- main.py
+++ main.py
@@ -136,93 +136,95 @@ meter_data = st.file_uploader(
 )
 
 if is_vending_data_uploaded:
+    try:
+        st.header("Detection of Cyber Attack on Cummulative Energy Usage")
+        st.write("For any meter, two reading are being taken. The first reading is the cummulative energy usage of customers")
+        st.write("This reading starts from when the meter is installed till the current time. This reading is asynchronously sent to power stations to monitor energy usage of customers")
+        st.write("Anytime the cummulative energy usage reading for a customer reduces, this is an indication of anomaly")
 
-    st.header("Detection of Cyber Attack on Cummulative Energy Usage")
-    st.write("For any meter, two reading are being taken. The first reading is the cummulative energy usage of customers")
-    st.write("This reading starts from when the meter is installed till the current time. This reading is asynchronously sent to power stations to monitor energy usage of customers")
-    st.write("Anytime the cummulative energy usage reading for a customer reduces, this is an indication of anomaly")
+        if meter_data:
 
-    if meter_data:
+            meter_readings_df = pd.DataFrame()
+            for uploaded_file in meter_data:
+                dataset = pd.read_excel(uploaded_file, sheet_name=None)
+                input_df = dataset[list(dataset.keys())[-1]]
 
-        meter_readings_df = pd.DataFrame()
-        for uploaded_file in meter_data:
-            dataset = pd.read_excel(uploaded_file, sheet_name=None)
-            input_df = dataset[list(dataset.keys())[-1]]
+                if "ENERGY(KWH)" in input_df.columns:
+                        input_df.rename(columns={"ENERGY(KWH)": "Energy Reading(kwh)"}, inplace=True)
 
-            if "ENERGY(KWH)" in input_df.columns:
-                    input_df.rename(columns={"ENERGY(KWH)": "Energy Reading(kwh)"}, inplace=True)
+                if "CONSUMPTION(KWH)" in input_df.columns:
+                    input_df.rename(columns={"CONSUMPTION(KWH)": "Meter Units(kwh)"}, inplace=True)
 
-            if "CONSUMPTION(KWH)" in input_df.columns:
-                input_df.rename(columns={"CONSUMPTION(KWH)": "Meter Units(kwh)"}, inplace=True)
+                if "Consumption(kwh)" in input_df.columns:
+                    input_df.rename(columns={"Consumption(kwh)": "Meter Units(kwh)"}, inplace=True)
 
-            if "Consumption(kwh)" in input_df.columns:
-                input_df.rename(columns={"Consumption(kwh)": "Meter Units(kwh)"}, inplace=True)
+                assert "Meter SN" in input_df.columns, "Each file should contain the column `Meter SN` which is the meter number"
+                assert "Frozen Time" in input_df.columns, "Each file should contain the column `Frozen Time` which is the time the reading was recorded"
+                assert "Energy Reading(kwh)" in input_df.columns, "Each file should contain the column `ENERGY(KWH)` or `Energy Reading(kwh)` which is the Cummulative energy reading"
+                assert "Meter Units(kwh)" in input_df.columns, "Each file should contain the column `CONSUMPTION(KWH)` or `Consumption(kwh)` which is the residual units on the meter"
 
-            assert "Meter SN" in input_df.columns, "Each file should contain the column `Meter SN` which is the meter number"
-            assert "Frozen Time" in input_df.columns, "Each file should contain the column `Frozen Time` which is the time the reading was recorded"
-            assert "Energy Reading(kwh)" in input_df.columns, "Each file should contain the column `ENERGY(KWH)` or `Energy Reading(kwh)` which is the Cummulative energy reading"
-            assert "Meter Units(kwh)" in input_df.columns, "Each file should contain the column `CONSUMPTION(KWH)` or `Consumption(kwh)` which is the residual units on the meter"
+                input_df = input_df[["Meter SN", "Frozen Time", "Energy Reading(kwh)", "Meter Units(kwh)"]]
 
-            input_df = input_df[["Meter SN", "Frozen Time", "Energy Reading(kwh)", "Meter Units(kwh)"]]
+                if not meter_readings_df.shape[0]:
+                    meter_readings_df = input_df
+                else:
+                    meter_readings_df = pd.concat([meter_readings_df, input_df], ignore_index=True)
 
-            if not meter_readings_df.shape[0]:
-                meter_readings_df = input_df
-            else:
-                meter_readings_df = pd.concat([meter_readings_df, input_df], ignore_index=True)
+            meter_readings_df["Frozen Time"] = pd.to_datetime(meter_readings_df["Frozen Time"])
+            meter_readings_df["Month"] = meter_readings_df["Frozen Time"].dt.month_name()
 
-        meter_readings_df["Frozen Time"] = pd.to_datetime(meter_readings_df["Frozen Time"])
-        meter_readings_df["Month"] = meter_readings_df["Frozen Time"].dt.month_name()
+            meter_readings_df.sort_values(by=["Meter SN", "Frozen Time"], inplace=True)
+            meter_readings_df.reset_index(drop=True, inplace=True)
 
-        meter_readings_df.sort_values(by=["Meter SN", "Frozen Time"], inplace=True)
-        meter_readings_df.reset_index(drop=True, inplace=True)
+            st.write(meter_readings_df.head())
 
-        st.write(meter_readings_df.head())
+            cumm_usage_anomaly, detailed_cumm_usage_anomaly = check_cumm_usage_diff(meter_readings_df)
 
-        cumm_usage_anomaly, detailed_cumm_usage_anomaly = check_cumm_usage_diff(meter_readings_df)
+            st.write("**Cummulative Usage Anomalies**")
+            st.write(cumm_usage_anomaly)
 
-        st.write("**Cummulative Usage Anomalies**")
-        st.write(cumm_usage_anomaly)
+            st.write("**Detailed Cummulative Usage Anomaly**")
+            st.write(detailed_cumm_usage_anomaly)
 
-        st.write("**Detailed Cummulative Usage Anomaly**")
-        st.write(detailed_cumm_usage_anomaly)
+            @st.cache_data
+            def download_cumm_anomalies_data(df):
+                return df.to_csv(index=False).encode("utf-8")
 
-        @st.cache_data
-        def download_cumm_anomalies_data(df):
-            return df.to_csv(index=False).encode("utf-8")
+            cumm_anomaly_download_file = download_cumm_anomalies_data(detailed_cumm_usage_anomaly)
 
-        cumm_anomaly_download_file = download_cumm_anomalies_data(detailed_cumm_usage_anomaly)
+            st.download_button(
+                label="Download Detailed Cummulative Usage Anomaly Data",
+                data=cumm_anomaly_download_file,
+                file_name="Cummulative Usage Anomalies.csv",
+                mime="text/csv",
+                help="Click this button to download data of cummulative usage anomaly occurrences as a csv file"
+                )
+            
+        st.header("Detection of Cyber Attack on Monthly Energy Usage")
+        st.write("Sometimes customers may tamper with meters to increase the residual units left on their meters")
+        st.write("This final step is to detect such acts")
+        st.write("The monthly usage of customers is gotten from the difference of the cummulative energy usage")
+        st.write("When the monthly usage of customers is subtracted from the sum of the energy units the customer has at the beginning of the month and the amount of units the customer is credited in that month, this should be the amount of units left at the beginning of the next month")
+        st.write("Anything contrary to this should be flagged as an anomaly")
+            
+        if meter_data:
+            monthly_usage_anomaly = check_monthly_usage(meter_readings_df, expected_df)
 
-        st.download_button(
-            label="Download Detailed Cummulative Usage Anomaly Data",
-            data=cumm_anomaly_download_file,
-            file_name="Cummulative Usage Anomalies.csv",
-            mime="text/csv",
-            help="Click this button to download data of cummulative usage anomaly occurrences as a csv file"
-            )
-        
-    st.header("Detection of Cyber Attack on Monthly Energy Usage")
-    st.write("Sometimes customers may tamper with meters to increase the residual units left on their meters")
-    st.write("This final step is to detect such acts")
-    st.write("The monthly usage of customers is gotten from the difference of the cummulative energy usage")
-    st.write("When the monthly usage of customers is subtracted from the sum of the energy units the customer has at the beginning of the month and the amount of units the customer is credited in that month, this should be the amount of units left at the beginning of the next month")
-    st.write("Anything contrary to this should be flagged as an anomaly")
-        
-    if meter_data:
-        monthly_usage_anomaly = check_monthly_usage(meter_readings_df, expected_df)
+            st.write("**Monthly Usage Anomaly**")
+            st.write(monthly_usage_anomaly)
 
-        st.write("**Monthly Usage Anomaly**")
-        st.write(monthly_usage_anomaly)
+            @st.cache_data
+            def download_monthly_usage_anomalies_data(df):
+                return df.to_csv(index=False).encode("utf-8")
 
-        @st.cache_data
-        def download_monthly_usage_anomalies_data(df):
-            return df.to_csv(index=False).encode("utf-8")
+            monthly_usage_anomaly_download_file = download_monthly_usage_anomalies_data(monthly_usage_anomaly)
 
-        monthly_usage_anomaly_download_file = download_monthly_usage_anomalies_data(monthly_usage_anomaly)
-
-        st.download_button(
-            label="Download Monthly Usage Anomaly Data",
-            data=cumm_anomaly_download_file,
-            file_name="Monthly Usage Anomalies.csv",
-            mime="text/csv",
-            help="Click this button to download data of monthly usage anomaly occurrences as a csv file"
-            )
+            st.download_button(
+                label="Download Monthly Usage Anomaly Data",
+                data=cumm_anomaly_download_file,
+                file_name="Monthly Usage Anomalies.csv",
+                mime="text/csv",
+                help="Click this button to download data of monthly usage anomaly occurrences as a csv file"
+                )
+    except Exception as e:
+        st.error(e)
