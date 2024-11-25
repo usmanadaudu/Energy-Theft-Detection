d27d0538c36582d9d718cb3ba265cfc4d71b79f7
usmanadaudu <adekunleakorede101@gmail.com>
2024-11-25 14:13:05 +0100

    adding function to convert Dataframe to excel file

--- utils.py
+++ utils.py
@@ -119,3 +119,20 @@ def get_anomalies_df_for_download(df, month_list):
     output_df = output_df.loc[output_df["Anomaly"], output_column_names]
     
     return output_df
+
+def to_excel(df):
+    import pandas as pd
+    from io import BytesIO
+    # from pyxlsb import open_workbook as open_xlsb
+
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+
+    return processed_data
