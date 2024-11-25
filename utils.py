3f23952ac00441a95991d9dd73804684905cbb30
usmanadaudu <adekunleakorede101@gmail.com>
2024-11-25 12:22:28 +0100

    added function to get expected credit units per payment

--- utils.py
+++ utils.py
@@ -19,3 +19,45 @@ def get_tariff_rate(band):
 
     return tariff_rate
 
+def get_expected_units(df):
+    month_list = ["May", "June", "July", "Aug", "Sept"]
+    permissible_error = 5    # amount of allowable error in percentage
+    output_df = df.copy()
+
+    for month in month_list:
+        month_payment_column_name = " ".join([month, "Naira"])
+
+        expected_units_column_name_5_5 = " ".join(["Expected Units",month,
+                                                   "(5.5 VAT)"])
+        lower_bound_column_name_5_5 = " ".join(["Lower Bound", month,
+                                                "(5.5 VAT)"])
+        upper_bound_column_name_5_5 = " ".join(["Upper Bound", month,
+                                                "(5.5 VAT)"])
+
+        expected_units_column_name_7_5 = " ".join(["Expected Units",month,
+                                                   "(7.5 VAT)"])
+        lower_bound_column_name_7_5 = " ".join(["Lower Bound", month,
+                                                "(7.5 VAT)"])
+        upper_bound_column_name_7_5 = " ".join(["Upper Bound", month,
+                                                "(7.5 VAT)"])
+
+        vat_charge_7_5 = 0.075 * df[month_payment_column_name]
+        expected_units_7_5 = ((df[month_payment_column_name] - vat_charge_7_5)
+                            / df["TARIFF_RATE"])
+        output_df[expected_units_column_name_7_5] = expected_units_7_5
+        output_df[lower_bound_column_name_7_5] = ((1 - permissible_error/100)
+                                              * output_df[expected_units_column_name_7_5])
+        output_df[upper_bound_column_name_7_5] = ((1 + permissible_error/100)
+                                              * output_df[expected_units_column_name_7_5])
+
+        vat_charge_5_5 = 0.055 * df[month_payment_column_name]
+        expected_units_5_5 = ((df[month_payment_column_name] - vat_charge_5_5)
+                            / df["TARIFF_RATE"])
+        output_df[expected_units_column_name_5_5] = expected_units_5_5
+        output_df[lower_bound_column_name_5_5] = ((1 - permissible_error/100)
+                                              * output_df[expected_units_column_name_5_5])
+        output_df[upper_bound_column_name_5_5] = ((1 + permissible_error/100)
+                                              * output_df[expected_units_column_name_5_5])
+
+    return output_df
+
