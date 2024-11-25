08c7de1e538a58ada7c51efe14e95e5bc49896be
usmanadaudu <adekunleakorede101@gmail.com>
2024-11-25 10:23:23 +0100

    explaining project aspects

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import streamlit as st
 st.title("Detection of Energy Theft Through Cyber Attack")
 
 st.write("This app implements a system for detecting energy theft which are orchestrated through cyber attack")
+
+st.write("""
+This app detects three types of cyber attacks
+
+1. Cyber attack on energy unit purchases (getting more units for the amount paid)
+2. Cyber attack on cummulative energy usage reading on meter
+3. Cyber attck on meter residual units
+""")
+
+st.header("Detection of Cyber Attack on Energy Unit Purchases")
+st.write("")
