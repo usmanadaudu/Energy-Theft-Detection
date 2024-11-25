54008ea62c23aa2d103862585a92e5dcf3d7d037
usmanadaudu <adekunleakorede101@gmail.com>
2024-11-25 10:06:05 +0100

    adding intros to app

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("Detection of Energy Theft Through Cyber Attack")
+
+st.write("This app implements a system for detecting energy theft which are orchestrated through cyber attack")
